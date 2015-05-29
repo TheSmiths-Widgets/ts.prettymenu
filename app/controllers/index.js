606b02feced9c37816c5f0725fad1d28baa8d694
Matthias Benkort <matthias.benkort@gmail.com>
2015-05-29 21:07:00 +0200

    Add example app

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,40 @@
-function doClick(e) {
-    alert($.label.text);
+$.prettyMenu.init(
+    [
+        { id: "profile", icon: "fa-eye", title: "Mijn profiel", onClick: handleClick },
+        { id: "question", icon: "fa-question", title: "Gestelde vragen", onClick: handleClick },
+        { id: "help", icon: "fa-info", title: "Help", onClick: handleClick },
+        { id: "dunno", icon: "fa-flag-o", title: "UitLoggen", onClick: handleClick  }
+    ], { width: 100 }
+);
+
+
+$.prettyMenu2.init(
+    [
+        { id: "menu", icon: "fa-question" }, 
+        { id: "menu", icon: "fa-futbol-o" }, 
+        { id: "menu", icon: "fa-gamepad" }, 
+        { id: "menu", icon: "fa-heart" }, 
+        { id: "menu", icon: "fa-cloud" },
+        { id: "menu", icon: "fa-train" }, 
+        { id: "menu", icon: "fa-bank" }, 
+        { id: "menu", icon: "fa-github" }, 
+        { id: "menu", icon: "fa-apple" }, 
+        { id: "menu", icon: "fa-dlfjgdlfkjg" }
+    ], {
+        backgroundColor: "#9ACD32",
+        foregroundColor: "#FFFFFF",
+        perRow: 5,
+        horizontalMargin: 0,
+        verticalMargin: 0,
+        innerPadding: 0,
+        outerPadding: 12.5,
+        iconSize: 25,
+        width: 50
+    }
+);
+
+function handleClick(clickEvent) {
+    Ti.API.warn(clickEvent);
 }
 
 $.index.open();
